c8cb76eac406fc9e027158ea42b67b62f459cdbe
Unknown025 <11259556+Unknown025@users.noreply.github.com>
2023-04-18 19:06:30 -0700

    Add Election model.

---
+++ Backend/Evoting_Backend/Evoting_Backend/Models/Election.cs
@@ -0,0 +1,10 @@
+﻿namespace Evoting_Backend.Models;
+
+public class Election
+{
+    public int ElectionId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string? PoliticalParty { get; set; }
+    public string? Location { get; set; }
+}
